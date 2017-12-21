Ziv-Barber/officegen

commit 74e04fafe664f07c4012b77f4002a64e3e276e2a

Move the paragraph API to a new file
The docx paragraph API now moved into it's own file so we can reuse it.

lib/docx-p.js
@@ -1,0 +1,219 @@
+//
+// officegen: paragraph API for docx
+//
+// Please refer to README.md for this module's documentations.
+//
+// NOTE:
+// - Before changing this code please refer to the hacking the code section on README.md.
+//
+// Copyright (c) 2013-2017 Ziv Barber;
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// 'Software'), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+var fast_image_size = require('fast-image-size');
+
+/**
+ * This function implementing the paragraph API for docx based documents.
+ *
+ * @param {object} genobj The document object to work on it.
+ * @param {object} genPrivate Access to the internals of this object.
+ * @param {string} docType Must be the same as the new_type parameter to the document generator's constructor.
+ * @param {object} dataContainer Optional container to store the new object.
+ * @param {object} extraSettings Optional extra settings for the paragraph API itself.
+ * @param {object} options Paragraph options.
+ * @summary Implementation of the pptx document plugins system so it'll be easier to add new features.
+ * @constructor
+ * @name MakeDocxP
+ */
+function MakeDocxP ( genobj, genPrivate, docType, dataContainer, extraSettings, options ) {
+	// Save everything because we'll need it later:
+	this.docType = docType;
+	this.genPrivate = genPrivate;
+	this.ogPluginsApi = genPrivate.plugs;
+	this.genobj = genobj;
+	this.data = [];
+	this.extraSettings = extraSettings || {};
+	this.options = options || {};
+
+	if ( dataContainer ) {
+		dataContainer.push ( this );
+	} // Endif.
+
+	return this;
+}
+
+/**
+ * Insert text inside this paragraph.
+ *
+ * @param {string} text_msg The text message itself.
+ * @param {object} opt ???.
+ * @param {object} flag_data ???.
+ */
+MakeDocxP.prototype.addText = function ( text_msg, opt, flag_data ) {
+	var newP = this;
+	var objNum = newP.data.length
+	newP.data[objNum] = { text: text_msg, options: opt || {}, ext_data: flag_data };
+
+	if ((opt || {}).link) {
+		var link_rel_id = newP.genPrivate.type.msoffice.rels_app.length + 1;
+
+		newP.genPrivate.type.msoffice.rels_app.push({
+			type: 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink',
+			target: opt.link,
+			targetMode: 'External'
+		})
+
+		newP.data[objNum].link_rel_id = link_rel_id;
+	}
+};
+
+/**
+ * Insert a line break inside this paragraph.
+ */
+MakeDocxP.prototype.addLineBreak = function () {
+	var newP = this;
+	newP.data[newP.data.length] = { 'line_break': true };
+};
+
+/**
+ * Insert a horizontal line inside this paragraph.
+ */
+MakeDocxP.prototype.addHorizontalLine = function () {
+	var newP = this;
+	newP.data[newP.data.length] = { 'horizontal_line': true };
+};
+
+/**
+ * Insert an image into the current paragraph.
+ *
+ * @param {object} image_path The image file to add.
+ * @param {object} opt Additional options (cx, cy).
+ * @param {object} image_format_type ???.
+ */
+MakeDocxP.prototype.addImage = function ( image_path, opt, image_format_type ) {
+	var newP = this;
+	var image_type = (typeof image_format_type == 'string') ? image_format_type : 'png';
+	var defWidth = 320;
+	var defHeight = 200;
+
+	if ( typeof image_path == 'string' ) {
+		var ret_data = fast_image_size ( image_path );
+		if ( ret_data.type == 'unknown' ) {
+			var image_ext = path.extname ( image_path );
+
+			switch ( image_ext ) {
+				case '.bmp':
+					image_type = 'bmp';
+					break;
+
+				case '.gif':
+					image_type = 'gif';
+					break;
+
+				case '.jpg':
+				case '.jpeg':
+					image_type = 'jpeg';
+					break;
+
+				case '.emf':
+					image_type = 'emf';
+					break;
+
+				case '.tiff':
+					image_type = 'tiff';
+					break;
+			} // End of switch.
+
+		} else {
+			if ( ret_data.width ) {
+				defWidth = ret_data.width;
+			} // Endif.
+
+			if ( ret_data.height ) {
+				defHeight = ret_data.height;
+			} // Endif.
+
+			image_type = ret_data.type;
+			if ( image_type == 'jpg' ) {
+				image_type = 'jpeg';
+			} // Endif.
+		} // Endif.
+	} // Endif.
+
+	var objNum = newP.data.length;
+	newP.data[objNum] = { image: image_path, options: opt || {} };
+
+	if ( !newP.data[objNum].options.cx && defWidth ) {
+		newP.data[objNum].options.cx = defWidth;
+	} // Endif.
+
+	if ( !newP.data[objNum].options.cy && defHeight ) {
+		newP.data[objNum].options.cy = defHeight;
+	} // Endif.
+
+	var image_id = newP.genPrivate.type.msoffice.src_files_list.indexOf ( image_path );
+	var image_rel_id = -1;
+
+	if ( image_id >= 0 ) {
+		for ( var j = 0, total_size_j = newP.genPrivate.type.msoffice.rels_app.length; j < total_size_j; j++ ) {
+			if ( newP.genPrivate.type.msoffice.rels_app[j].target == ('media/image' + (image_id + 1) + '.' + image_type) ) {
+				image_rel_id = j + 1;
+			} // Endif.
+		} // Endif.
+
+	} else
+	{
+		image_id = newP.genPrivate.type.msoffice.src_files_list.length;
+		newP.genPrivate.type.msoffice.src_files_list[image_id] = image_path;
+		newP.genPrivate.plugs.intAddAnyResourceToParse ( 'word\\media\\image' + (image_id + 1) + '.' + image_type, (typeof image_path == 'string') ? 'file' : 'stream', image_path, null, false );
+	} // Endif.
+
+	if ( image_rel_id == -1 ) {
+		image_rel_id = newP.genPrivate.type.msoffice.rels_app.length + 1;
+
+		newP.genPrivate.type.msoffice.rels_app.push (
+			{
+				type: 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/image',
+				target: 'media/image' + (image_id + 1) + '.' + image_type,
+				clear: 'data'
+			}
+		);
+	} // Endif.
+
+	if ((opt || {}).link) {
+
+		var link_rel_id = newP.genPrivate.type.msoffice.rels_app.length + 1;
+
+		newP.genPrivate.type.msoffice.rels_app.push({
+			type: 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink',
+			target: opt.link,
+			targetMode: 'External'
+		});
+
+		newP.data[objNum].link_rel_id = link_rel_id;
+
+	} // Endif.
+
+	newP.data[objNum].image_id = image_id;
+	newP.data[objNum].rel_id = image_rel_id;
+};
+
+module.exports = MakeDocxP;

lib/gendocx.js
@@ -34,16 +34,16 @@
 
 var baseobj = require("./basicgen.js");
 var msdoc = require("./msofficegen.js");
+var docxP = require('./docx-p.js');
 var docxTable = require('./docxtable.js');
 var xmlBuilder = require('xmlbuilder');
 
 var path = require('path');
 
-var fast_image_size = require('fast-image-size');
-
 var docplugman = require('./docplug');
 
 // Officegen docx plugins:
+// var plugHeadfoot = require('./docxplg-headfoot');
 // BMK_DOCX_PLUG:
 
 if ( !String.prototype.encodeHTML ) {
@@ -157,7 +157,10 @@ function makeDocx ( genobj, new_type, options, gen_private, type_info ) {
 	 * @return Text string.
 	 */
 	function cbMakeDocxDocument ( data ) {
-		var outString = gen_private.plugs.type.msoffice.cbMakeMsOfficeBasicXml ( data ) + '<w:document xmlns:ve="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:w10="urn:schemas-microsoft-com:office:word" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:wne="http://schemas.microsoft.com/office/word/2006/wordml"><w:body>';
+		data.docStartExtra = data.docStartExtra || '';
+		data.docEndExtra = data.docEndExtra || '';
+
+		var outString = gen_private.plugs.type.msoffice.cbMakeMsOfficeBasicXml ( data ) + '<w:' + data.docType + ' xmlns:ve="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:w10="urn:schemas-microsoft-com:office:word" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:wne="http://schemas.microsoft.com/office/word/2006/wordml">' + data.docStartExtra;
 		var objs_list = data.data;
 
 		// Work on all the stored paragraphs inside this document:
@@ -374,31 +377,38 @@ function makeDocx ( genobj, new_type, options, gen_private, type_info ) {
 			outString += '</w:p>';
 		} // End of for loop.
 
-		outString += '<w:p w:rsidR="00A02F19" w:rsidRDefault="00A02F19"/>';
+		if ( data.docType === 'document' ) {
+			outString += '<w:p w:rsidR="00A02F19" w:rsidRDefault="00A02F19"/>';
 
-		// Landscape orientation support:
-		if ( options.orientation && options.orientation === 'landscape' ){
-			outString +=
-				'<w:sectPr w:rsidR="00A02F19" w:rsidSect="00897086">' +
-				'<w:pgSz w:w="15840" w:h="12240" w:orient="landscape"/>' +
-				'<w:pgMar w:top="1800" w:right="1440" w:bottom="1800" w:left="1440" w:header="720" w:footer="720" w:gutter="0"/>' +
-				'<w:cols w:space="720"/>' +
-				'<w:docGrid w:linePitch="360"/>' +
-				'</w:sectPr>';
-		} else {
-			outString +=
-				'<w:sectPr w:rsidR="00A02F19" w:rsidSect="00A02F19">' +
-				'<w:pgSz w:w="12240" w:h="15840"/>' +
-				'<w:pgMar w:top="1440" w:right="1800" w:bottom="1440" w:left="1800" w:header="720" w:footer="720" w:gutter="0"/>' +
-				'<w:cols w:space="720"/>' +
-				'<w:docGrid w:linePitch="360"/>' +
-				'</w:sectPr>';
+			// Landscape orientation support:
+			if ( options.orientation && options.orientation === 'landscape' ){
+				outString +=
+					'<w:sectPr w:rsidR="00A02F19" w:rsidSect="00897086">' +
+					(docxData.secPrExtra ? docxData.secPrExtra : '') +
+					'<w:pgSz w:w="15840" w:h="12240" w:orient="landscape"/>' +
+					'<w:pgMar w:top="1800" w:right="1440" w:bottom="1800" w:left="1440" w:header="720" w:footer="720" w:gutter="0"/>' +
+					'<w:cols w:space="720"/>' +
+					'<w:docGrid w:linePitch="360"/>' +
+					'</w:sectPr>';
+			} else {
+				outString +=
+					'<w:sectPr w:rsidR="00A02F19" w:rsidSect="00A02F19">' +
+					(docxData.secPrExtra ? docxData.secPrExtra : '') +
+					'<w:pgSz w:w="12240" w:h="15840"/>' +
+					'<w:pgMar w:top="1440" w:right="1800" w:bottom="1440" w:left="1800" w:header="720" w:footer="720" w:gutter="0"/>' +
+					'<w:cols w:space="720"/>' +
+					'<w:docGrid w:linePitch="360"/>' +
+					'</w:sectPr>';
+			} // Endif.
 		} // Endif.
 
-		outString += '</w:body></w:document>';
+		outString += data.docEndExtra + '</w:' + data.docType + '>';
 
 		return outString;
 	}
+
+	// Save it so if some plugin need to generate document style resource then it can use it:
+	genobj.cbMakeDocxDocument = cbMakeDocxDocument;
 
 	// Prepare genobj for MS-Office:
 	msdoc.makemsdoc ( genobj, new_type, options, gen_private, type_info );
@@ -422,6 +432,7 @@ function makeDocx ( genobj, new_type, options, gen_private, type_info ) {
 	var plugsmanObj = new docplugman ( genobj, gen_private, 'docx', setDefaultDocValues );
 
 	// We'll register now any officegen internal plugin that we want to always use for Word based documents:
+	// plugsmanObj.plugsList.push ( new plugHeadfoot ( plugsmanObj ) );
 	// BMK_DOCX_PLUG:
 
 	// Dynamic loading of additional plugins requested by the user:
@@ -443,6 +454,9 @@ function makeDocx ( genobj, new_type, options, gen_private, type_info ) {
 			plugsmanObj.plugsList.push ( new newPlug ( plugsmanObj ) );
 		});
 	} // Endif.
+
+	// Access to our data:
+	var docxData = plugsmanObj.getDataStorage ();
 
 	gen_private.type.msoffice.files_list.push (
 		{
@@ -517,7 +531,7 @@ function makeDocx ( genobj, new_type, options, gen_private, type_info ) {
 	gen_private.plugs.intAddAnyResourceToParse ( 'word\\settings.xml', 'buffer', null, cbMakeDocxSettings, true );
 	gen_private.plugs.intAddAnyResourceToParse ( 'word\\webSettings.xml', 'buffer', null, cbMakeDocxWeb, true );
 	gen_private.plugs.intAddAnyResourceToParse ( 'word\\styles.xml', 'buffer', null, cbMakeDocxStyles, true );
-	gen_private.plugs.intAddAnyResourceToParse ( 'word\\document.xml', 'buffer', genobj, cbMakeDocxDocument, true );
+	gen_private.plugs.intAddAnyResourceToParse ( 'word\\document.xml', 'buffer', { docType: 'document', docStartExtra: '<w:body>', docEndExtra: '</w:body>', data: genobj.data }, cbMakeDocxDocument, true );
 	gen_private.plugs.intAddAnyResourceToParse ( 'word\\numbering.xml', 'buffer', null, cbMakeDocxNumbers, true ); // added to support bullets
 
 	gen_private.plugs.intAddAnyResourceToParse ( 'word\\_rels\\document.xml.rels', 'buffer', gen_private.type.msoffice.rels_app, gen_private.plugs.type.msoffice.cbMakeRels, true );
@@ -527,169 +541,11 @@ function makeDocx ( genobj, new_type, options, gen_private, type_info ) {
 	/**
 	 * Create a new paragraph.
 	 *
-	 * @param[in] options Default options for all the objects inside this paragraph.
+	 * @param {string} options Default options for all the objects inside this paragraph.
 	 */
 	genobj.createP = function ( options ) {
-		var newP = {};
-
-		newP.data = [];
-		newP.options = options || {};
-
-		/**
-		 * Insert text inside this paragraph.
-		 *
-		 * @param {string} text_msg The text message itself.
-		 * @param {object} opt ???.
-		 * @param {object} flag_data ???.
-		 */
-		newP.addText = function ( text_msg, opt, flag_data ) {
-			var objNum = newP.data.length
-			newP.data[objNum] = { text: text_msg, options: opt || {}, ext_data: flag_data };
-
-			if ((opt || {}).link) {
-				var link_rel_id = gen_private.type.msoffice.rels_app.length + 1;
-
-				gen_private.type.msoffice.rels_app.push({
-					type: 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink',
-					target: opt.link,
-					targetMode: 'External'
-				})
-
-				newP.data[objNum].link_rel_id = link_rel_id;
-			}
-		};
-
-		/**
-		 * Insert a line break inside this paragraph.
-		 */
-		newP.addLineBreak = function () {
-			newP.data[newP.data.length] = { 'line_break': true };
-		};
-
-		/**
-		 * Insert a horizontal line inside this paragraph.
-		 */
-		newP.addHorizontalLine = function () {
-      		newP.data[newP.data.length] = { 'horizontal_line': true };
-		};
-
-		/**
-		 * Insert an image into the current paragraph.
-		 *
-		 * @param {object} image_path The image file to add.
-		 * @param {object} opt Additional options (cx, cy).
-		 * @param {object} image_format_type ???.
-		 */
-		newP.addImage = function ( image_path, opt, image_format_type ) {
-			var image_type = (typeof image_format_type == 'string') ? image_format_type : 'png';
-			var defWidth = 320;
-			var defHeight = 200;
-
-			if ( typeof image_path == 'string' ) {
-				var ret_data = fast_image_size ( image_path );
-				if ( ret_data.type == 'unknown' ) {
-					var image_ext = path.extname ( image_path );
-
-					switch ( image_ext ) {
-						case '.bmp':
-							image_type = 'bmp';
-							break;
-
-						case '.gif':
-							image_type = 'gif';
-							break;
-
-						case '.jpg':
-						case '.jpeg':
-							image_type = 'jpeg';
-							break;
-
-						case '.emf':
-							image_type = 'emf';
-							break;
-
-						case '.tiff':
-							image_type = 'tiff';
-							break;
-					} // End of switch.
-
-				} else {
-					if ( ret_data.width ) {
-						defWidth = ret_data.width;
-					} // Endif.
-
-					if ( ret_data.height ) {
-						defHeight = ret_data.height;
-					} // Endif.
-
-					image_type = ret_data.type;
-					if ( image_type == 'jpg' ) {
-						image_type = 'jpeg';
-					} // Endif.
-				} // Endif.
-			} // Endif.
-
-			var objNum = newP.data.length;
-			newP.data[objNum] = { image: image_path, options: opt || {} };
-
-			if ( !newP.data[objNum].options.cx && defWidth ) {
-				newP.data[objNum].options.cx = defWidth;
-			} // Endif.
-
-			if ( !newP.data[objNum].options.cy && defHeight ) {
-				newP.data[objNum].options.cy = defHeight;
-			} // Endif.
-
-			var image_id = gen_private.type.msoffice.src_files_list.indexOf ( image_path );
-			var image_rel_id = -1;
-
-			if ( image_id >= 0 ) {
-				for ( var j = 0, total_size_j = gen_private.type.msoffice.rels_app.length; j < total_size_j; j++ ) {
-					if ( gen_private.type.msoffice.rels_app[j].target == ('media/image' + (image_id + 1) + '.' + image_type) ) {
-						image_rel_id = j + 1;
-					} // Endif.
-				} // Endif.
-
-			} else
-			{
-				image_id = gen_private.type.msoffice.src_files_list.length;
-				gen_private.type.msoffice.src_files_list[image_id] = image_path;
-				gen_private.plugs.intAddAnyResourceToParse ( 'word\\media\\image' + (image_id + 1) + '.' + image_type, (typeof image_path == 'string') ? 'file' : 'stream', image_path, null, false );
-			} // Endif.
-
-			if ( image_rel_id == -1 ) {
-				image_rel_id = gen_private.type.msoffice.rels_app.length + 1;
-
-				gen_private.type.msoffice.rels_app.push (
-					{
-						type: 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/image',
-						target: 'media/image' + (image_id + 1) + '.' + image_type,
-						clear: 'data'
-					}
-				);
-			} // Endif.
-
-			if ((opt || {}).link) {
-
-				var link_rel_id = gen_private.type.msoffice.rels_app.length + 1;
-
-				gen_private.type.msoffice.rels_app.push({
-					type: 'http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink',
-					target: opt.link,
-					targetMode: 'External'
-				});
-
-				newP.data[objNum].link_rel_id = link_rel_id;
-
-			} // Endif.
-
-			newP.data[objNum].image_id = image_id;
-			newP.data[objNum].rel_id = image_rel_id;
-		};
-
-		genobj.data[genobj.data.length] = newP;
-
-		return newP;
+		// Create a new instance of the paragraph object:
+		return new docxP ( genobj, gen_private, 'docx', genobj.data, {}, options );
 	};
 
 	/**

lib/docplug.js
@@ -56,13 +56,14 @@ function clearDocData ( docpluginman ) {
  * you must create an instance of this object.
  *
  * @param {object} genobj The document object to work on it.
- * @param {object} gen_private Access to the internals of this object.
+ * @param {object} genPrivate Access to the internals of this object.
  * @param {string} docType Must be the same as the new_type parameter to the document generator's constructor.
+ * @param {function} defValuesFunc The function to use to reset the document's data.
  * @summary Implementation of the pptx document plugins system so it'll be easier to add new features.
  * @constructor
- * @name makedocpluginapi
+ * @name MakeDocPluginapi
  */
-function makedocpluginapi ( genobj, genPrivate, docType, defValuesFunc ) {
+function MakeDocPluginapi ( genobj, genPrivate, docType, defValuesFunc ) {
 	// Save everything because we'll need it later:
 	this.docType = docType;
 	this.genPrivate = genPrivate;
@@ -84,13 +85,14 @@ function makedocpluginapi ( genobj, genPrivate, docType, defValuesFunc ) {
 
 	// Catch the clear document data event and connect it to us:
 	genobj.on ( 'clearDocType', clearDocData ( this ) );
+	return this;
 }
 
 /**
  * Check for verbose mode for this document type.
  * @param {string} moduleName Optional, Allow filtering by feature / module.
  */
-makedocpluginapi.prototype.getVerboseMode = function ( moduleName ) {
+MakeDocPluginapi.prototype.getVerboseMode = function ( moduleName ) {
 	return this.ogPluginsApi.getVerboseMode ( this.docType, moduleName );
 }
 
@@ -98,7 +100,7 @@ makedocpluginapi.prototype.getVerboseMode = function ( moduleName ) {
  * Allow displaying a verbose messages to the console.
  * @param {string} message The message to display.
  */
-makedocpluginapi.prototype.logIfVerbose = function ( message ) {
+MakeDocPluginapi.prototype.logIfVerbose = function ( message ) {
 	if ( this.getVerboseMode () ) {
 		console.log ( message );
 	} // Endif.
@@ -108,7 +110,7 @@ makedocpluginapi.prototype.logIfVerbose = function ( message ) {
  * Get the object to install features related to this document type.
  * @return The features object.
  */
-makedocpluginapi.prototype.getFeaturesStorage = function () {
+MakeDocPluginapi.prototype.getFeaturesStorage = function () {
 	return this.genPrivate.features.type[this.docType];
 }
 
@@ -116,7 +118,7 @@ makedocpluginapi.prototype.getFeaturesStorage = function () {
  * Get the object to place data used by this document.
  * @return The document's data object.
  */
-makedocpluginapi.prototype.getDataStorage = function () {
+MakeDocPluginapi.prototype.getDataStorage = function () {
 	return this.genPrivate.type[this.docType];
 }
 
@@ -125,7 +127,7 @@ makedocpluginapi.prototype.getDataStorage = function () {
  * @param{string} eventType Type of event to catch.
  * @param{function} cbFunc Callback function.
  */
-makedocpluginapi.prototype.registerCallback = function ( eventType, cbFunc ) {
+MakeDocPluginapi.prototype.registerCallback = function ( eventType, cbFunc ) {
 	// First make sure that we have a list of callbacks for this event type:
 	if ( !this.callbacksList[eventType] ) {
 		this.callbacksList[eventType] = [];
@@ -140,7 +142,7 @@ makedocpluginapi.prototype.registerCallback = function ( eventType, cbFunc ) {
  * @param{string} eventType Type of event to emit.
  * @param{*} eventData Additional optional data.
  */
-makedocpluginapi.prototype.emitEvent = function ( eventType, eventData ) {
+MakeDocPluginapi.prototype.emitEvent = function ( eventType, eventData ) {
 	var funcThis = this;
 
 	// We'll do something only if we have this type of event:
@@ -153,4 +155,4 @@ makedocpluginapi.prototype.emitEvent = function ( eventType, eventData ) {
 	} // Endif.
 };
 
-module.exports = makedocpluginapi;
+module.exports = MakeDocPluginapi;

lib/genpptx.js
@@ -48,6 +48,7 @@ var docplugman = require('./docplug');
 // Officegen pptx plugins:
 var plugWidescreen = require('./pptxplg-widescreen');
 var plugSpeakernotes = require('./pptxplg-speakernotes');
+// var plugLayouts = require('./pptxplg-layouts');
 // BMK_PPTX_PLUG:
 
 if ( !String.prototype.encodeHTML ) {
@@ -1352,6 +1353,7 @@ function makePptx ( genobj, new_type, options, gen_private, type_info ) {
 	// We'll register now any specific PowerPoint based plugin that we want to use:
 	plugsmanObj.plugsList.push ( new plugWidescreen ( plugsmanObj ) );
 	plugsmanObj.plugsList.push ( new plugSpeakernotes ( plugsmanObj ) );
+	// plugsmanObj.plugsList.push ( new plugLayouts ( plugsmanObj ) );
 	// BMK_PPTX_PLUG:
 
 	// Dynamic loading of additional plugins requested by the user:

plugins/templates/pptx/pptxplg-example.js
@@ -21,9 +21,9 @@
  * @summary Create a new pptx plugin.
  * @param {object} pluginsman Access to the plugins manager for pptx documents.
  * @constructor
- * @name makeexampleplugin
+ * @name MakeExamplePlugin
  */
-function makeexampleplugin ( pluginsman ) {
+function MakeExamplePlugin ( pluginsman ) {
 	var funcThis = this;
 
 	// You can change it if you want to support more types, since that the Word and Excel document generators also supporting very similar plugins:
@@ -77,7 +77,7 @@ function makeexampleplugin ( pluginsman ) {
  * This function extending the main document object with new API methods.
  * @param {object} docObj Document object.
  */
-makeexampleplugin.prototype.extendPptxApi = function ( docObj ) {
+MakeExamplePlugin.prototype.extendPptxApi = function ( docObj ) {
 	var funcThis = this;
 
 	/*
@@ -110,7 +110,7 @@ makeexampleplugin.prototype.extendPptxApi = function ( docObj ) {
  * This function extending a new created slide object with new API methods.
  * @param {object} docData Object with information about the new slide and more.
  */
-makeexampleplugin.prototype.extendPptxSlideApi = function ( docData ) {
+MakeExamplePlugin.prototype.extendPptxSlideApi = function ( docData ) {
 	var funcThis = this;
 	var newSlide = docData.page; // The new slide's API.
 	var slideData = docData.pageData; // Place here data related to the slide.
@@ -147,7 +147,7 @@ makeexampleplugin.prototype.extendPptxSlideApi = function ( docData ) {
  * This function should initialize the default specific data that this plugin needs for the document.
  * @param {object} docObj Document object.
  */
-makeexampleplugin.prototype.setData = function ( docObj ) {
+MakeExamplePlugin.prototype.setData = function ( docObj ) {
 	var funcThis = this;
 
 	// BMK_TODO:
@@ -157,7 +157,7 @@ makeexampleplugin.prototype.setData = function ( docObj ) {
  * This function been called just before starting to generate the output document zip.
  * @param {object} docObj Document object.
  */
-makeexampleplugin.prototype.beforeGen = function ( docObj ) {
+MakeExamplePlugin.prototype.beforeGen = function ( docObj ) {
 	var funcThis = this;
 
 	// this.pluginsman.genPrivate.pages = access to all the slides (in case that you need it).
@@ -212,7 +212,7 @@ makeexampleplugin.prototype.beforeGen = function ( docObj ) {
  */
 /*
 // Example:
-makeexampleplugin.prototype.cbMyCallback = function ( data ) {
+MakeExamplePlugin.prototype.cbMyCallback = function ( data ) {
 	return this.msPluginsApi.cbMakeMsOfficeBasicXml ( data ) + '<example></example>';
 }
 */
@@ -223,4 +223,4 @@ makeexampleplugin.prototype.cbMyCallback = function ( data ) {
 
 // --- just put it here ---
 
-module.exports = makeexampleplugin;
+module.exports = MakeExamplePlugin;

commit eb0e1bf8da3b1b0c30433efc0a67f5b347930e7a

Fix missing require('path')
This bug was introduced by 74e04fafe664f07c4012b77f4002a64e3e276e2a when
the paragraph code was moved into it's own file. This accounts for the
unused require('path') in gendocx.js

lib/docx-p.js
@@ -29,6 +29,7 @@
 //
 
 var fast_image_size = require('fast-image-size');
+var path = require('path');
 
 /**
  * This function implementing the paragraph API for docx based documents.

lib/gendocx.js
@@ -38,8 +38,6 @@ var docxP = require('./docx-p.js');
 var docxTable = require('./docxtable.js');
 var xmlBuilder = require('xmlbuilder');
 
-var path = require('path');
-
 var docplugman = require('./docplug');
 
 // Officegen docx plugins: